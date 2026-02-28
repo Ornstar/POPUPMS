5fbb20cbb644742d80117f3b89b0d610d63d47e9
bot <manaakumana51@gmail.com>
2026-02-28 13:57:47 +0700

    Update popupms.js

--- popupms.js
+++ popupms.js
@@ -3,7 +3,7 @@
 const CONFIG = {
   LIVECHAT_URL: "https://urlmsshorten.com/livechat-mauslot",
   TELE_URL: "https://urlmsshorten.com/group-tele-official",
-  IMAGE_URL: "https://i.postimg.cc/d11h5SKp/f3b6e26d-321c-4c54-85c8-5dfcaaafa594.jpg",
+  IMAGE_URL: "https://i.postimg.cc/tTCSR6KJ/MS009-Promotion.jpg",
   STYLE_ID: "MAUSLOT_style_IMLEK_FX",
   OVERLAY_ID: "MAUSLOTOv",
   CLOSE_ID: "MAUSLOTClose",
